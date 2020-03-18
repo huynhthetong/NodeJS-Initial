a2b73685ccd1fd298323ea78ef1b912f655f81b2
Stephen-Bill <huynhthetong1999999@gmail.com>
2020-03-18 18:52:41 +0700

    Complete step-4 652PM31820

--- server/server.js
+++ server/server.js
@@ -1,66 +1,77 @@
-// find modules
 const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const _ = require('lodash')
+const morgan = require('morgan')
 
-const app = express()
-
+// default PORT and host
 const PORT = 3001
 const host = '127.0.0.1'
 
-app.use(express.static('client'))
+var lions = []
+var id = 0
 
+var updatedId = (req, res, next) => {
+  if (!req.body.id) {
+    id++
+    req.body.id = id + ''
+  }
+  next()
+}
+
+app.use(morgan('dev'))
+app.use(express.static('client'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-var lions = []
-var id = 0
-// TODO: Make the REST  routes to perform CRUD on lions
+app.param('id', (req, res, next, id) => {
+  var lion = _find(lions, { id })
+
+  if (lion) {
+    req.lion = lion
+    next()
+  } else {
+    res.send
+  }
+})
+
 app.get('/lions', (req, res) => {
-  res.json(lions)
+  res.json()
 })
 
 app.get('/lions/:id', (req, res) => {
-  var lion = _.find(lions, { id: req.params.id })
+  var lion = req.lion
   res.json(lion || {})
 })
 
-app.post('/lions', (req, res) => {
+app.post('/lions', updatedId, (req, res) => {
   var lion = req.body
-  id++
-  lion.id = id + ''
+
   lions.push(lion)
 
-  res.json(lions)
+  res.json(lion)
 })
 
-app.put('/lions/:id', function(req, res) {
+app.put('/lions/:id', (req, res) => {
   var update = req.body
-  if (update.id) {
-    delete update.id
-  }
+  if (update.id) delete update.id
 
-  var lion = _.findIndex(lions, { id: req.params.id })
+  var lion = _find(lions, { id: req.param.id })
+
   if (!lions[lion]) {
     res.send()
   } else {
-    var updatedLion = _.assign(lions[lion], update)
+    var updatedLion = _.assign(lions[lions], update)
     res.json(updatedLion)
   }
 })
 
-app.delete('/lions/:id', (req, res) => {
-  var lion = _.find(lions, { id: req.params.id })
-
-  if (!lions[lion]) {
-    res.send()
-  } else {
-    var deletedLion = lions[lion]
-    lions.splice(lion, 1)
-    res.json(deletedLion)
-  }
+// Middle ware error handler
+app.use((err, req, res, next) => {
+  if (err) res.status(500).send(err)
 })
 
+// listen
 app.listen(PORT, host, () => {
-  console.log(`Server running on port ${PORT} at http://${host}:${PORT}/`)
+  console.log(`Server running on ${PORT} at http://${host}:${PORT}`)
 })
